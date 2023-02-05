e13c37890940b0b47d590fc62cb2eb2b380704c4
joel Assagou <liamstiduotechnology@gmail.com>
2023-02-05 06:26:55 +0000

    first commit

--- validation.js
+++ validation.js
@@ -1,6 +1,6 @@
 
 function validationLogin(){
-    const email = document.getElementById("email").value;
+    const email = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
     if(email === "test@gmail.com" && password === "pass@111"){        
@@ -15,13 +15,20 @@ function validationLogin(){
 function validationRegister(){
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
-    const fname = document.getElementById("f-name").value;
-    const lname = document.getElementById("l-name").value;
+    const fname = document.getElementById("fname").value;
+    const lname = document.getElementById("lname").value;
 
     if(email === "" || password === ""  || fname === "" || lname === ""){
         alert("fill all the field before submitting")
     }else{        
         alert("Register successfully");
+        
     }
 }
 
+
+function loginBtn(){
+    document.getElementById("login").onclick = function(){
+            location.href = "/login"
+    };
+}
